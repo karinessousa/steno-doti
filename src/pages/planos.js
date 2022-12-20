7111bc81ddf20a2fa5e7abc12481b68737984d41
Steno <solutionssteno@gmail.com>
2022-12-20 10:07:33 -0300

    atualizada a pagina de planos

--- src/pages/planos.js
+++ src/pages/planos.js
@@ -18,20 +18,20 @@ function Planos ()  {
               <PlanoComponent 
                 nome={'Básico'} 
                 descricao={'Para equipes pequenas de até 6 pessoas'} 
-                lista={'Limitado a 6 perfis/Missões ilimitadas/1 Painel gerente/Armazenamento de 1gb/Limitado a 2 salas/Limitação de personagens/Limitação de itens/Monitoramento de tempo'}
-                preco={'R$ 39.90'}  
+                lista={'Limitado a 6 perfis/Missões ilimitadas/1 Painel gerente/Armazenamento de 1gb/Limitado a 2 salas/Limitação de personagens/Limitação de itens'}
+                preco={'R$ 0.00'}  
               />
               <PlanoComponent 
-                nome={'Premium 01'} 
+                nome={'Premium'} 
                 descricao={'Para equipes médias de até 30 pessoas'} 
-                lista={'Limitado a 30 perfis/Missões ilimitadas/5 Painel gerente/Armazenamento de 4gb/Limitado a 8 salas/Monitoramento de tempo/Mudar algumas regras'}
-                preco={'R$ 215.90'}  
+                lista={'Limitado a 30 perfis/Missões ilimitadas/5 Painéis gerentes/Armazenamento de 4gb/Limitado a 7 salas/Regras personalizadas'}
+                preco={'R$ 59.90'}  
               />
               <PlanoComponent 
                 nome={'Premium Plus'} 
                 descricao={'Para equipes grandes de até 60 pessoas'} 
-                lista={'60 perfis/missões ilimitadas/10 painel de gerente/Armazenamento 8gb/20 salas/monitoramento de tempo/criador de regras personalizadas'}
-                preco={'R$ 430.90'}  
+                lista={'60 perfis/missões ilimitadas/10 painéis gerentes/Armazenamento 8gb/12 salas/Regras personalizadas'}
+                preco={'R$ 115.90'}  
               />
             </div>
           </div>
