5eee62ec4e3dd1d8ac32cb3398041d132bb93b98
Karine Sousa <ksousa27@gmail.com>
2022-12-19 22:25:16 -0300

    commit

---
+++ src/components/game.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+function Game() {
+  return (
+    <canvas>
+      
+    </canvas>
+  );
+}
+
+export default Game;
