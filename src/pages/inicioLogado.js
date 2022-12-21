33610955f0b7be275a729c1f3c444e7a0c9e7a7e
Karine Sousa <ksousa27@gmail.com>
2022-12-20 23:12:10 -0300

    commit

--- src/pages/inicioLogado.js
+++ src/pages/inicioLogado.js
@@ -10,50 +10,52 @@ function InicioLogado() {
     <div>
       <MenuLogado />
       <div className='inicioLogado'>
-        <div className='menuEsquerdo'>
-            <div className='logoEmpresa'></div>
-            <h3>Nome Empresa</h3>
-            <h4>CNPJ</h4>
-            <p>XX.XXX.XXX/0001-XX</p>
-            <h4>Cargo</h4>
-            <p>Júnior 2</p>
-            <h4>Lider responsável</h4>
-            <p>André Gonçalves</p>
+        <div className='corpoLogado'>
+          <div className='menuEsquerdo'>
+              <div className='logoEmpresa'></div>
+              <h3>Nome Empresa</h3>
+              <h4>CNPJ</h4>
+              <p>XX.XXX.XXX/0001-XX</p>
+              <h4>Cargo</h4>
+              <p>Júnior 2</p>
+              <h4>Lider responsável</h4>
+              <p>André Gonçalves</p>
 
-            <ul>
-                <li>Missões Pendentes</li>
-                <li>Documentação</li>
-                <li>Painel Gerente</li>
-                <li>Histórico</li>
-                <li>Personagem</li>
-            </ul>
+              <ul>
+                  <li>Missões Pendentes</li>
+                  <li>Documentação</li>
+                  <li>Painel Gerente</li>
+                  <li>Histórico</li>
+                  <li>Personagem</li>
+              </ul>
 
-            <h4>Recompensas</h4>
-            <div className='barraProgresso'>
-                <div className='progresso'></div>
-            </div>
+              <h4>Recompensas</h4>
+              <div className='barraProgresso'>
+                  <div className='progresso'></div>
+              </div>
 
-            <table>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </table>
+              <table>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+                  <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                  </tr>
+              </table>
 
-        </div>
-        <div className='colunaDireita'>
-            <div className='missoesPendentes'>
+          </div>
+          <div className='colunaDireita'>
+              <div className='missoesPendentes'>
 
-            </div>
-            <div className='game'>
+              </div>
+              <div className='game'>
 
-            </div>
+              </div>
+          </div>
         </div>
       </div>
       <Rodape/>
