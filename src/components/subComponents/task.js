08805a245108cbe89d9942c8d042b480c0130d92
Karine Sousa <ksousa27@gmail.com>
2023-01-15 19:23:12 -0300

    commit

--- src/components/subComponents/task.js
+++ src/components/subComponents/task.js
@@ -2,10 +2,12 @@ import '../../App.css';
 import React from 'react';
 // import { Link } from 'react-router-dom';
 
-function Task() {
+function Task(props) {
   return (
     <div className='task'>
-      
+      <div className='check'></div>
+      <h5>{props.taskName}</h5>
+      <h5 className='data'>22/01</h5>
     </div>
   );
 }
